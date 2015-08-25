c33678c1d3230863d2dbf217f32654c390a2b85e
Rene Tianco <mysterio2004173@gmail.com>
2015-08-25 16:21:29 -0400

    First commit

---
+++ src/filter.py
@@ -0,0 +1,82 @@
+from pattern.en import wordlist
+import sys, os, re
+
+class Filter(object):
+
+    def __init__(self, queries, grams):
+        self._queries = queries
+        self._grams = grams
+        try:
+            data = '../data/'
+            self._cities = [w.decode('utf-8', 'ignore') for w in open(data + 'city.lst').read().upper().split('\n')]
+            self._countries = [w.decode('utf-8', 'ignore') for w in open(data + 'country.lst').read().upper().split('\n')]
+            self._males = [w.decode('utf-8', 'ignore') for w in open(data + 'person_male.lst').read().split('\n')]
+            self._females = [w.decode('utf-8', 'ignore') for w in open(data + 'person_female.lst').read().split('\n')]
+        except:
+            print "Error reading list files"
+
+    def isCity(self, w):
+        return w.upper() in self._cities
+
+    def isCountry(self, w):
+        return w.upper() in self._countries
+
+    def isLocation(self, w):
+        return self.isCity(w) or self.isCountry(w)
+
+    def isTime(self, w):
+        return w.lower() in wordlist.TIME
+
+    def isNumber(self, w):
+        return w.isdigit()
+
+    def isUpper(self, w):
+        return w[0].isupper()
+
+    def isName(self, w):
+        return w in self._males or w in self._females
+
+    def isNovel(self, w):
+        return w.lower() not in self._queries[-1][0].lower().strip('?').split()
+
+    def isNotNovel(self, w):
+        return not self.isNovel(w)
+
+    def reweightGrams(self):
+        orig_q = self._queries[-1][0]
+        checks = [self.isNovel] # improve the score of any grams that meet these conditions
+        strips = [self.isNotNovel] # decrease the scores of any grams that meet these conditions
+        if re.search('when', orig_q, re.IGNORECASE):
+            checks += [self.isTime, self.isNumber]
+            strips += [self.isName, self.isLocation]
+        elif re.search('who', orig_q, re.IGNORECASE):
+            checks += [self.isName, self.isUpper]
+            strips += [self.isLocation]
+        elif re.search('where', orig_q, re.IGNORECASE):
+            checks += [self.isUpper, self.isLocation]
+            strips += [self.isName, self.isTime]
+        elif re.search("how (old|many|much)", orig_q, re.IGNORECASE):
+            checks += [self.isNumber]
+            strips += [self.isName, self.isLocation]
+        elif re.search("(which|what).*?(country|countries)", orig_q, re.IGNORECASE):
+            checks += [self.isUpper, self.isCountry]
+            strips += [self.isName]
+        elif re.search("(which|what).*?(city|cities)", orig_q, re.IGNORECASE):
+            checks += [self.isUpper, self.isCity]
+            strips += [self.isName]
+        elif re.search("(which|what)", orig_q, re.IGNORECASE):
+            strips += [self.isName]
+        for g in self._grams:
+            for f in checks:
+                passed = 0
+                for w in g:
+                    if f(w):
+                        passed += 1
+                self._grams[g] *= (1 + passed)
+            for f in strips:
+                failed = 0
+                for w in g:
+                    if f(w):
+                        failed += 1
+                self._grams[g] /= (3 + failed)
+        return self._grams
---
+++ src/search.py
@@ -0,0 +1,34 @@
+import engine, query, filter, tile
+import argparse
+
+def search(queryinput, key, provider, cutoff, n):
+    e = engine.Engine(provider, key)
+
+    q = query.Query(queryinput)
+    qs = q.getQueries()
+
+    g = e.searchAndGram(qs)
+    f = filter.Filter(qs, g)
+    f.reweightGrams()
+
+    t = tile.Tile(g)
+    return t.getAnswers(cutoff, n)
+
+def main():
+    desc = "QA system using Google/Bing as an information backend"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('query', help='Question to be answered.')
+    parser.add_argument('-k', '--key', default=None,
+            help='API key for chosen backend provider')
+    parser.add_argument('-p', '--provider', default='bing',
+            help='choose Bing or Google as a backend, defaults to Bing')
+    parser.add_argument('-c', '--cutoff',  default=10, type=int,
+            help='cut off for top ranked ngrams to be tiled for the final answer, defaults to 10')
+    parser.add_argument('-n', '--nanswers', default=1, type=int,
+            help='returns the top n answers (must be less than the cutoff value), defaults to 1')
+    args = parser.parse_args()
+
+    q, k, p, c, n = args.query, args.key, args.provider, args.cutoff, args.nanswers
+    print search(q, k, p, c, n)
+
+if __name__ == "__main__": main()
